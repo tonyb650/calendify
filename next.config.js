89f0ef1f7266e1afc2cf581ecf14da499ee93476
Tony Brierly <anthonylbrierly@gmail.com>
2026-05-27 10:57:30 -0700

    Test deployment without prisma nextjs-monorepo-workaround-plugin

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
-  // next.config.js
-    const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-    /** @type {import('next').NextConfig} */
-    const nextConfig = {
-      webpack: (config, { isServer }) => {
-        if (isServer) {
-          config.plugins = [...config.plugins, new PrismaPlugin()];
-        }
-        return config;
-      },
-    };
-
-    module.exports = nextConfig;
+module.exports = nextConfig;
